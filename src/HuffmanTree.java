a9fd6a7df6c14a4ce7dba13179479e6fed778b9d
mkirwin <kirwinma@grinnell.edu>
2016-11-28 21:48:13 -0600

    Added "implements Comparable<Object>" thing to Node class.

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,11 +1,35 @@
+import java.io.File;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
 
 public class HuffmanTree {
+	private Node root;
+	private Map<Short, Integer> cur; //?
+	private File f = new File("example.txt");
 	
 	/** Constructs a new Huffman Tree using a given Map
 	 * @param m
+	 * @citation stackoverflow.com/questions/1066589/iterate-through-a-hashmap
 	 */
 	public HuffmanTree(Map<Short, Integer> m) {
+		cur = m;
+		PriorityQueue<Node> temp = null;
+		Iterator<Entry<Short, Integer>> iter = m.entrySet().iterator();
+		Node n;
+		// 1. Iterate through map.
+		while (iter.hasNext()) {
+			@SuppressWarnings("unchecked")
+			Map.Entry<Short, Integer> pair = 
+					(Map.Entry<Short, Integer>) iter.next();
+
+			n = new Node(pair.getKey(), pair.getValue());
+			
+		}
+		
+		// 2. Take each item from map and create a node.
+		// 3. Insert the node into the priority queue
 		
 	}
 	
--- src/Node.java
+++ src/Node.java
@@ -1,11 +1,43 @@
 
-public class Node {
+public class Node implements Comparable<Object> {
 	private Short ch;
 	private int freq;
 	private Node left;
 	private Node right;
 	
-	public Node() {
-		
+	/** Used to construct leaf nodes
+	 */
+	public Node(Short ch, int freq) {
+		this.ch = ch;
+		this.freq = freq;
+		left = null;
+		right = null;
+	}
+	
+	/** Additional constructor takes left and right node arguments
+	 * Used for interior nodes
+	 *
+	 */
+	public Node(int freq, Node left, Node right) {
+		this.ch = null;
+		this.freq = freq;
+		this.left = left;
+		this.right = right;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Node) {
+			Node other = (Node) o;
+			if (other.freq > freq) {
+				return other.freq - freq;
+			} else if (other.freq < freq) {
+				return freq - other.freq;
+			} else {
+				return 0;
+			}
+		} else {
+			throw new IllegalArgumentException();
+		}
 	}
 }
