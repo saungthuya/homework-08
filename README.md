a6068226d65822126a07313cb5fa282221d0f1ce
Maddie Kirwin <kirwinma@grinnell.edu>
2016-11-28 19:47:39 -0600

    Base files created.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # homework-08
 Draft of homework 8
+Maddie Kirwin & Saung Thuya
---
+++ src/HuffmanTree.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+
+public class HuffmanTree {
+	
+	/** Constructs a new Huffman Tree using a given Map
+	 * @param m
+	 */
+	public HuffmanTree(Map<Short, Integer> m) {
+		
+	}
+	
+	void encode(BitInputStream in, BitOutputStream out) {
+		
+	}
+	
+	void decode(BitInputStream in, BitOutputStream out) {
+		
+	}
+
+}
